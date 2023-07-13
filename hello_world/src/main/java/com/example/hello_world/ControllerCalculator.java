c97ca53d135ebbb93789c853fe239b2d0ad8c8aa
Matheus <48444913+maj-oliveira@users.noreply.github.com>
2023-07-13 14:06:23 -0300

    adding a controller

---
+++ hello_world/src/main/java/com/example/hello_world/ControllerCalculator.java
@@ -0,0 +1,9 @@
+package com.example.hello_world;
+
+import com.example.hello_world.Parameters;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class ControllerCalculator {
+}
---
+++ hello_world/src/main/java/com/example/hello_world/Parameters.java
@@ -0,0 +1,42 @@
+package com.example.hello_world;
+
+public class Parameters {
+
+    private double initialAmountMoney;
+    private double monthlyContribution;
+    private double interestRate;
+    private double yearsInvesting;
+
+
+    public double getInitialAmountMoney() {
+        return initialAmountMoney;
+    }
+
+    public void setInitialAmountMoney(double initialAmountMoney) {
+        this.initialAmountMoney = initialAmountMoney;
+    }
+
+    public double getMonthlyContribution() {
+        return monthlyContribution;
+    }
+
+    public void setMonthlyContribution(double monthlyContribution) {
+        this.monthlyContribution = monthlyContribution;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double getYearsInvesting() {
+        return yearsInvesting;
+    }
+
+    public void setYearsInvesting(double yearsInvesting) {
+        this.yearsInvesting = yearsInvesting;
+    }
+}
