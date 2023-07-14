6b5cfe7793e454777624747cda951fd7f6ad4829
Matheus <48444913+maj-oliveira@users.noreply.github.com>
2023-07-14 14:47:05 -0300

    trying to add a first endpoint

--- hello_world/src/main/java/com/example/hello_world/ControllerCalculator.java
+++ hello_world/src/main/java/com/example/hello_world/ControllerCalculator.java
@@ -3,7 +3,22 @@ package com.example.hello_world;
 import com.example.hello_world.Parameters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ControllerCalculator {
+
+    @GetMapping("/") //this annotations is used to nao HTTP GET request to a method
+    public String index(Model model){
+        model.addAttribute("parameters", new Parameters());
+        return "index";
+    }
+
+    @PostMapping("/Calculate")
+    public String calculate(Parameters parameters, Model model){
+        model.addAttribute("parameters", parameters);
+        model.addAttribute("calculated", true);
+        return "index";
+    }
 }
--- hello_world/src/main/java/com/example/hello_world/HelloWorldApplication.java
+++ hello_world/src/main/java/com/example/hello_world/HelloWorldApplication.java
@@ -2,12 +2,19 @@ package com.example.hello_world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello Spring";
+	}
 }
