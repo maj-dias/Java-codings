09d0c3cedc7b6782d08116b38fd9be452eac1dbf
Matheus <48444913+maj-oliveira@users.noreply.github.com>
2023-08-11 18:31:46 -0300

    almost finished the controller

--- hello_world/src/main/java/com/example/hello_world/ControllerCalculator.java
+++ hello_world/src/main/java/com/example/hello_world/ControllerCalculator.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+// If you want to make some calculation, you should put it in the controller file (here). The controller file is
+// responsible for handling HTTP requests and sending responses. It is a good place to put logic that needs to be
+// executed when a user interacts with your application.
 @Controller //In Spring’s approach to building web sites, HTTP requests are handled by a controller
 public class ControllerCalculator {
 
@@ -16,10 +19,20 @@ public class ControllerCalculator {
         return "index"; //return the name of a view
     }
 
+    //The @ModelAttribute annotation tells Spring MVC to bind the values of the parameters object
+    //to the parameters model attribute
     @PostMapping("/calculate") // "/calculate" is a endpoint
     public String calculateSubmit(@ModelAttribute Parameters parameters, Model model){
         //a variavel parameters com p minusculo será utilizada no html
+        System.out.println("-----------POST-------------");
+
+        double finalAmount = 0;
+        for (int i = 0; i < parameters.getYearsInvesting()*12; i++){
+            finalAmount = finalAmount* parameters.getInterestRate()^(1.0/12) + parameters.getMonthlyContribution();
+        }
+
         model.addAttribute("parameters", parameters);
+        model.addAttribute("finalAmount", finalAmount);
         model.addAttribute("calculated", true);
         return "result";
     }
